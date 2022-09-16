a67e3913d3628641d99b430d6c84f5323d194a2a
Arturo Jamaica <me@arturojamaica.com>
2022-09-16 11:23:01 -0500

    Add visibility for beet extensions

--- Sources/Beet/Extensions/BytesEncodable.swift
+++ Sources/Beet/Extensions/BytesEncodable.swift
@@ -1,22 +1,22 @@
 import Foundation
 
-protocol BytesEncodable {
+public protocol BytesEncodable {
     var bytes: [UInt8] { get }
 }
 
 extension UInt8: BytesEncodable {
-    var bytes: [UInt8] { [self] }
+    public var bytes: [UInt8] { [self] }
 }
 
 extension UInt32: BytesEncodable {
-    var bytes: [UInt8] {
+    public var bytes: [UInt8] {
         var littleEndian = self.littleEndian
         return withUnsafeBytes(of: &littleEndian) { Array($0) }
     }
 }
 
 extension UInt64: BytesEncodable {
-    var bytes: [UInt8] {
+    public var bytes: [UInt8] {
         var littleEndian = self.littleEndian
         return withUnsafeBytes(of: &littleEndian) { Array($0) }
     }
@@ -39,17 +39,17 @@ extension Data: BytesEncodable {
 }
 
 extension Bool: BytesEncodable {
-    var bytes: [UInt8] {self ? [UInt8(1)]: [UInt8(0)]}
+    public var bytes: [UInt8] {self ? [UInt8(1)]: [UInt8(0)]}
 }
 
 extension Array: BytesEncodable where Element == BytesEncodable {
-    var bytes: [UInt8] {reduce([], {$0 + $1.bytes})}
+    public var bytes: [UInt8] {reduce([], {$0 + $1.bytes})}
 }
 
 extension RawRepresentable where RawValue == UInt32 {
-    var bytes: [UInt8] { rawValue.bytes}
+    public var bytes: [UInt8] { rawValue.bytes}
 }
 
 extension RawRepresentable where RawValue == UInt8 {
-    var bytes: [UInt8] {rawValue.bytes}
+    public var bytes: [UInt8] {rawValue.bytes}
 }
