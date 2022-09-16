e5061df56f0e3d33586f883bb07741889b6012bc
Arturo Jamaica <me@arturojamaica.com>
2022-09-16 11:30:38 -0500

    Missing public

--- Sources/Beet/Vendor/Borsh/BorshEncoder.swift
+++ Sources/Beet/Vendor/Borsh/BorshEncoder.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public struct BorshEncoder {
-    func encode<T>(_ value: T) throws -> Data where T: BorshSerializable {
+    public func encode<T>(_ value: T) throws -> Data where T: BorshSerializable {
         var writer = Data()
         try value.serialize(to: &writer)
         return writer
