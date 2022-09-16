d5ebe8dad98b0714ea36e64fb0db44a6ba4599ba
Arturo Jamaica <me@arturojamaica.com>
2022-09-16 11:32:32 -0500

    Init decoders

--- Sources/Beet/Vendor/Borsh/BorshDecoder.swift
+++ Sources/Beet/Vendor/Borsh/BorshDecoder.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 public struct BorshDecoder {
+    public init() {}
     public func decode<T>(_ type: T.Type, from data: Data) throws -> T where T: BorshDeserializable {
         var reader = BinaryReader(bytes: [UInt8](data))
         return try T.init(from: &reader)
